c5d96767f1fc1e2aa91b821a9408f7bbfa22fcc9
Gaston <gaston.ignacio.gimenez@gmail.com>
2021-03-21 20:41:54 -0300

    resoluciojn parcial
    Se resolvio todos los ejercicios, excepto el de la funciona recursiva, revisar

--- practico_01/ejercicio_01.py
+++ practico_01/ejercicio_01.py
@@ -3,8 +3,11 @@
 
 def maximo_basico(a: float, b: float) -> float:
     """Toma dos números y devuelve el mayor.
-
     Restricción: No utilizar la función max"""
+    if a < b:
+        return b
+    else:
+        return a
     pass # Completar
 
 
@@ -21,6 +24,7 @@ def maximo_libreria(a: float, b: float) -> float:
     """Re-escribir utilizando el built-in max.
     Referencia: https://docs.python.org/3/library/functions.html#max
     """
+    return max(a, b)
     pass # Completar
 
 
@@ -37,6 +41,8 @@ def maximo_ternario(a: float, b: float) -> float:
     """Re-escribir utilizando el operador ternario.
     Referencia: https://docs.python.org/3/reference/expressions.html#conditional-expressions
     """
+    ok = a if (a > b) else b
+    return ok
     pass # Completar
 
 
--- practico_01/ejercicio_02.py
+++ practico_01/ejercicio_02.py
@@ -7,6 +7,15 @@ def maximo_encadenado(a: float, b: float, c: float) -> float:
     Restricción: Utilizar UNICAMENTE tres IFs y comparaciones encadenadas.
     Referencia: https://docs.python.org/3/reference/expressions.html#comparisons
     """
+    if a >= b:
+        if a >= c:
+            return a
+    elif b>= c:
+        return b
+    else:
+        return c
+
+
     pass # Completar
 
 
@@ -24,6 +33,7 @@ def maximo_cuadruple(a: float, b: float, c: float, d: float) -> float:
     """Re-escribir para que tome 4 parámetros, utilizar la función max.
 
     Referencia: https://docs.python.org/3/library/functions.html#max"""
+    return max(a,b,c,d)
     pass # Completar
 
 
@@ -39,9 +49,7 @@ assert maximo_cuadruple(24, 9, 18, 30) == 30
 
 
 def maximo_arbitrario(*args) -> float:
-    """Re-escribir para que tome una cantidad arbitraria de parámetros.
-    Referencia: https://docs.python.org/3/tutorial/controlflow.html#arbitrary-argument-lists
-    """
+    return max(args)
     pass # Completar
 
 
